92ae7c84361048b3681db401c3124f8360a04013
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2024-03-22 16:43:03 +0530

    Fixed pickle model import

--- scikit-learn/app.py
+++ scikit-learn/app.py
@@ -4,34 +4,23 @@ from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 import pickle
 
-model = pickle.load(open('model.pkl', 'rb')) #importing the ML model using pickle
+
 ## it says there is a FileNotFoundError with model.pkl file. 
 
-
 app = Flask(__name__)
-
-'''@app.route('/')
-def home():
-    return render_template('')#load link to the fornt end here'''
-
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json    # Get JSON data from the request
 
-
     irradiation = data['irradiation']
     module_temperature = data['module_temperature']     # Extract relevant features from JSON data
-
-
+    
     predicted_ac_output = model.predict([[irradiation, module_temperature]])     # Perform prediction using the loaded model
-
 
     return jsonify({'predicted_ac_output': predicted_ac_output[0]})     # Return predicted AC output as JSON response
 
-
-
 if __name__ == '__main__':
+    model = pickle.load(open('model.pkl', 'rb')) #importing the ML model using pickle
     app.run(debug=True)
 
